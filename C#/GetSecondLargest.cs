cfdaa705f27ee5df741c8efe99d73f7ed5a9c0f9
SantanuKMohapatra <167847555+SantanuKMohapatra@users.noreply.github.com>
2025-06-11 16:06:49 +0530

    Update GetSecondLargest(S).cs

--- C#/GetSecondLargest.cs
+++ C#/GetSecondLargest.cs
@@ -1,1 +1,36 @@
+class SecondLargest
+{
+    public static int GetSecondLargest(int[] arr)
+    {
+        int n = arr.Length;
 
+        if (n < 2)
+        {
+            return -1; // Or throw an exception
+        }
+
+        int largest = int.MinValue;
+        int secondLargest = int.MinValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > largest)
+            {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest)
+            {
+                secondLargest = arr[i];
+            }
+        }
+
+        // Handle case where all elements are same
+        if (secondLargest == int.MinValue)
+        {
+            return -1;
+        }
+
+        return secondLargest;
+    }
+}
